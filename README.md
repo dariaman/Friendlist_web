d94aa852ecd20701d02e1e2b373ec85ae4728c4a
Dariaman <dariaman.siagian@gmail.com>
2023-06-10 08:03:42 +0700

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# FriendlistWeb
+Angular CLI: 16.0.5
+Node: 18.16.0
+Package Manager: npm 9.7.1
+OS: win32 x64
+
+API : https://github.com/dariaman/Friendlist_api
---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FriendsComponent } from './friends/friends.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    FriendsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
